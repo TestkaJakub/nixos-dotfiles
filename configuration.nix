92209d386cc3c0d68a4192c8497dce6ce3e5c6e7
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-05 20:46:46 +0200

    upgrade 2025-10-05 20:46

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,7 @@ in
       backend = "glx";
       fade = true;
     };
-    services.udev.extraRules = ''
+    udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="leds", KERNEL=="tpacpi::kbd_backlight", \
       RUN+="${pkgs.coreutils}/bin/chmod 0666 /sys/class/leds/tpacpi::kbd_backlight/brightness"
   '';
