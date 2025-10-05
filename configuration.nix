d2d61249ce4d6678d4b663efbe4490447b4d8c1a
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-05 11:00:37 +0200

    upgrade 2025-10-05 11:00

--- configuration.nix
+++ configuration.nix
@@ -9,10 +9,10 @@ in
     ];
 
   home-manager = {
-    backupFileExtension = "backup";
+    users.jakub.backupFileExtension = "backup";
     useUserPackages = true;
     useGlobalPkgs = true;
-    users.jakub = import ./home.nix;
+    #users.jakub = import ./home.nix;
   };
 
   boot.loader = {
