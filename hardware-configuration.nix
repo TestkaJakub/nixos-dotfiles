187a99bd303a98a4e85456a4a49c9f94ee0e632a
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-06 00:27:10 +0200

    upgrade 2025-10-06 00:27

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -33,6 +33,7 @@
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.networkmanager.enable = true;
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
