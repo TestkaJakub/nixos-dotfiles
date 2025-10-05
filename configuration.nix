a3315fe93940ec2d82151287139b6499857d84e2
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-05 11:42:01 +0200

    upgrade 2025-10-05 11:42

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@ in
     ];
 
   home-manager = {
-    backupFileExtension = "backup";
+    backupFileExtension = builtins.readFile (pkgs.runCommand "timestamp" {} '' date --utc +%Y-%m-%d_%H-%M-%S > $out'');
     useUserPackages = true;
     useGlobalPkgs = true;
     users.jakub = import ./home.nix;
