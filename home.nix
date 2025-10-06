40330b2f5e7fa88d509221259a8bd12f85854315
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-06 12:34:58 +0200

    upgrade 2025-10-06 12:34

--- home.nix
+++ home.nix
@@ -101,7 +101,7 @@
     };
   };
 
- home.file.".config/hyprpaper".text = ''
+ xgd.configFile."hypr/hyprpaper.conf".text = ''
    preload = ~/Wallpapers/toradora.png
    wallpaper = , ~/Wallpapers/toradora.png
  '';
