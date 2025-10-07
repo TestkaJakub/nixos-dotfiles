34454689e812a99407b77a7eef2f4ec0ac0df318
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-07 23:39:54 +0200

    upgrade 2025-10-07 23:39

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,4 @@
-{ config, lib, pkgs, ... }:
-let
-  tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm/default.nix { };
-in
+{ config, pkgs, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -14,14 +11,9 @@ in
       ./services/udev.nix
       ./users/jakub.nix
       ./environment.nix
+      ./home-manager.nix
+      ./programs/hyprland.nix
     ];
-
-  home-manager = {
-    backupFileExtension = builtins.readFile (pkgs.runCommand "timestamp" {} '' date --utc +%Y-%m-%d_%H-%M-%S > $out'');
-    useUserPackages = true;
-    useGlobalPkgs = true;
-    users.jakub = import ./home.nix;
-  };
 
   nixpkgs.config.allowUnfree = true;
 
@@ -36,51 +28,6 @@ in
   };
 
   time.timeZone = "Europe/Warsaw";
-
-#  programs.firefox.enable = true;
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
-
-  # environment.systemPackages = with pkgs; [
-  #  (writeShellScriptBin "kbm" ''
-   #   path="/sys/class/leds/tpacpi::kbd_backlight/brightness"
-    #  max_path="/sys/class/leds/tpacpi::kbd_backlight/max_brightness"
-#
- #     if [ ! -w "$path" ]; then
-  #      echo "Error: cannot write to $path" >&2
-      #  exit 1
-     # fi
-
-     # cur=$(cat "$path" 2>/dev/null || echo 0)
-     # max=$(cat "$max_path" 2>/dev/null || echo 2)
-
-     # if [ "$#" -ge 1 ]; then
-       # val="$1"
-       # if [ "$val" -gt "$max" ]; then
-        #  val="$max"
-       # elif [ "$val" -lt 0 ]; then
-       #   val=0
-      #  fi
-     # else
-       # if ! [[ "$cur" =~ ^[0-9]+$ ]]; then
-        #  cur=0
-       # fi
-      #  val=$(( (cur + 1) % (max + 1) ))
-     # fi
-
-   #   echo "$val" > "$path"
-    #'')
-  #];
-
-  #environment.variables = {
-  #  PF_INFO = "ascii title os host kernel uptime pkgs memory";
-  #  PF_SOURCE = "";
-  #};
-
-  #environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   system.stateVersion = "25.05"; 
 }
