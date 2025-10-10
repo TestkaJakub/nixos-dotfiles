80f14cb287d0351bc46cba7851a28f0784068038
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-11 00:22:36 +0200

    upgrade 2025-10-11 00:22

--- wm/hyprland.nix
+++ wm/hyprland.nix
@@ -58,9 +58,9 @@
 	  "$mod SHIFT, 8, movetoworkspace, 8"
 	  "$mod SHIFT, 9, movetoworkspace, 9"
 
-	  "XF86AudioMute, exec, pamixer -t"
-          "XF86AudioLowerVolume, exec, pamixer --allow-boost -d 5"
-          "XF86AudioRaiseVolume, exec, pamixer --allow-boost -i 5"
+	  ",XF86AudioMute, exec, pamixer -t"
+          ",XF86AudioLowerVolume, exec, pamixer --allow-boost -d 5"
+          ",XF86AudioRaiseVolume, exec, pamixer --allow-boost -i 5"
 	];
         bindm = [
           "$mod, mouse:272, movewindow"
