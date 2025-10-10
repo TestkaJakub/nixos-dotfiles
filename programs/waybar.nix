11148150ed08758824121daa05393c399962e748
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-11 00:45:42 +0200

    upgrade 2025-10-11 00:45

--- programs/waybar.nix
+++ programs/waybar.nix
@@ -14,33 +14,33 @@
         modules-right = [ "custom/pamixer" "battery" "clock" ];
 
 	"custom/pamixer" = {
-                  interval = 2;
-        format = "{}";
-        return-type = "json";
+          interval = 2;
+          format = "{}";
+          return-type = "json";
 
-        exec = let
-          pamixerStatus = pkgs.writeShellApplication {
-            name = "pamixer-status";
-            runtimeInputs = [ pkgs.pamixer ];
-            checkPhase = "";  # disable ShellCheck
-            text = ''
-              volume=$(pamixer --get-volume)
-              muted=$(pamixer --get-mute)
+          exec = let
+            pamixerStatus = pkgs.writeShellApplication {
+              name = "pamixer-status";
+              runtimeInputs = [ pkgs.pamixer ];
+              checkPhase = "";  # disable ShellCheck
+              text = ''
+                volume=$(pamixer --get-volume)
+                muted=$(pamixer --get-mute)
 
-              if [ "$muted" = "true" ]; then
-                text="Muted"
-              else
-                text="Vol: $volume%"
-              fi
+                if [ "$muted" = "true" ]; then
+                  text="Muted"
+                else
+                  text="Vol: $volume%"
+                fi
 
-              echo "{\"text\": \"$text\", \"tooltip\": \"$text\"}"
-            '';
-          };
-        in "${pamixerStatus}/bin/pamixer-status";
+                echo "{\"text\": \"$text\", \"tooltip\": \"$text\"}"
+              '';
+            };
+          in "${pamixerStatus}/bin/pamixer-status";
 
-        on-click = "pamixer -t";
-        on-scroll-up = "pamixer -i 5";
-        on-scroll-down = "pamixer -d 5";
+          on-click = "pamixer -t";
+          on-scroll-up = "pamixer -i 5";
+          on-scroll-down = "pamixer -d 5";
         };
       };
     };
