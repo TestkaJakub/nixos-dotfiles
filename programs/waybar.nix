7b6dbd329a4386df6d34b431ecef99ca250608a8
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-11 00:29:44 +0200

    upgrade 2025-10-11 00:29

--- programs/waybar.nix
+++ programs/waybar.nix
@@ -9,7 +9,30 @@
     '';
     settings = {
       main = {
-        modules-right = [ "battery" "clock" ];
+        modules-right = [ "custom/pamixer" "battery" "clock" ];
+
+	"custom/pamixer" = {
+	  interval = 2;
+	  format = "audio {}%";
+	  return-type = "json";
+	  exec = "${pkgs.writeShellScript "pamixer-status.sh" ''
+	    volue=$(pamixer --get-volume)
+	    muted=$(pamixer --get-mute)
+
+	    if [ "$muted" = "true" ]; then
+              icon=""
+            elif [ "$volume" -lt 30 ]; then
+              icon=""
+            elif [ "$volume" -lt 70 ]; then
+              icon=""
+	    else
+              icon=""
+            fi
+
+            echo "{\"text\": \"$icon $volume%\", \"tooltip\": \"Volume: $volume%\"}"
+	  '';
+	  };
+	};
       };
     };
   };
