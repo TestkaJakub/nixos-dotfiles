a723329ba9b8d85e08a4f04ea72942ae3d8806e6
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-11 10:46:46 +0200

    upgrade 2025-10-11 10:46

--- services/swww.nix
+++ services/swww.nix
@@ -1,22 +1,4 @@
 { config, pkgs, ... }:
 {
   services.swww.enable = true;
-
-  # Automatically set your wallpaper after swww starts
-  systemd.user.services."swww-wallpaper" = {
-    Unit = {
-      Description = "Set initial wallpaper via swww";
-      After = [ "graphical-session.target" "swww.service" ];
-      Wants = [ "swww.service" ];
-    };
-    Service = {
-      ExecStart = ''
-        ${pkgs.swww}/bin/swww img /home/jakub/Wallpapers/makima.gif --transition-type grow --transition-step 90
-      '';
-      Type = "oneshot";
-    };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
-  };
 }
