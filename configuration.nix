84cc26f71367655b066a9a1367f20670bf64aafb
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-11 22:13:04 +0200

    upgrade 2025-10-11 22:13

--- configuration.nix
+++ configuration.nix
@@ -29,9 +29,9 @@
 
   environment.systemPackages = with pkgs; [
     (pkgs.runCommand "bzip2-compat-32" { buildInputs = [ pkgsi686Linux.bzip2 ]; } ''
-      mkdir -p $out/lib
+      mkdir -p $out/lib32
       target=$(ls ${pkgsi686Linux.bzip2.out}/lib/libbz2.so.1* | head -n1)
-      ln -s "$target" "$out/lib/libbz2.so.1.0"
+      ln -s "$target" "$out/lib32/libbz2.so.1.0"
     '')
   ];
 
@@ -49,12 +49,6 @@
       pulseaudio
       libvdpau
       bzip2
-
-      (pkgs.runCommand "bzip2-compat-32" { buildInputs = [ pkgsi686Linux.bzip2 ]; } ''
-        mkdir -p $out/lib
-        target=$(ls ${pkgsi686Linux.bzip2.out}/lib/libbz2.so.1* | head -n1)
-        ln -s $target $out/lib/libbz2.so.1.0
-      '') 
     ];
   };
 
