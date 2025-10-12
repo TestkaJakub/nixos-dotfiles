3a601e6fd3c24b7adfc91755efbed7720c1ed0dc
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-12 09:26:45 +0200

    upgrade 2025-10-12 09:26

---
+++ templates/android-studio/flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Template: Android development environment";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    templates.default = {
+      path = ./.;
+      description = "Android-studio development environment template";
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        android-studio
+	android-tools
+	gradle_7
+	glibc
+        git
+	zlib
+	ncurses5
+      gitRepo
+      gnupg
+      python2
+      curl
+      procps
+      openssl
+      gnumake
+      nettools
+      # For nixos < 19.03, use `androidenv.platformTools`
+      androidenv.androidPkgs_9_0.platform-tools
+      jdk
+      schedtool
+      util-linux
+      m4
+      gperf
+      perl
+      libxml2
+      zip
+      unzip
+      bison
+      flex
+      lzop
+      python3
+      ];
+      shellHook = ''
+        export ALLOW_NINJA_ENV=true
+    export USE_CCACHE=1
+    export ANDROID_JAVA_HOME=${pkgs.jdk.home}sdkmanager install avd
+    export LD_LIBRARY_PATH=/usr/lib:/usr/lib32
+      '';
+    };
+  };
+}
