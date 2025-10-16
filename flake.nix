28d90d693f52876cc86d73ce1e31d6c09a95176f
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-16 20:15:34 +0200

    upgrade 2025-10-16 20:15

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
 	  inherit inputs;
 	};
         modules = [
-	  mangowc.nixos.default
+	  mangowc.nixosModules.default
           ./configuration.nix
           home-manager.nixosModules.home-manager
         ];
