6f302c91eb72193ea1b08f4797fdb736b59d1959
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-25 01:11:22 +0200

    av

--- configuration/services.nix
+++ configuration/services.nix
@@ -1,15 +1,15 @@
 { pkgs, lib, config, user, ... }:
 
-let
-  tokyo-night-sddm =
-    pkgs.libsForQt5.callPackage ../tokyo-night-sddm/default.nix { };
-in
+#let
+#  tokyo-night-sddm =
+#    pkgs.libsForQt5.callPackage ../tokyo-night-sddm/default.nix { };
+#in
 {
   services.displayManager = {
     enable = true;
     sddm = {
       enable = true;
-      theme = "tokyo-night-sddm";
+      #theme = "tokyo-night-sddm";
       wayland.enable = true;
     };
   };
