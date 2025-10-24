ae26a0482dab80cf3f2766de6c6cd6360c2d097d
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-25 00:52:43 +0200

    man

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Welcome to my NixOs configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,10 +19,18 @@
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit system version user inputs; };
+
         modules = [
           ./main.nix
           home-manager.nixosModules.home-manager
-          { home-manager.users.${user} = import ./home.nix; }
+
+          {
+            home-manager.users.${user} =
+              import ./home.nix {
+                inherit pkgs system inputs user;
+              };
+          }
+
           mangowc.nixosModules.mango
         ];
       };
