bf7c1f0f223840ef9047458780c22cb8ddc3b548
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-26 13:26:31 +0100

    upgrade 2025-10-26 13:26

--- configuration/gaming.nix
+++ configuration/gaming.nix
@@ -1,21 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      mesa
+      libdrm
+    ];
+  };
+
   programs.steam = {
     enable = true;
-
     package = pkgs.steam.override {
       extraPkgs = pkgs: with pkgs; [
         mesa
-        mesa.drivers
         libdrm
       ];
     };
   };
 
-  # Modern graphics option names
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
+  # Optional but helps FHS sandbox compatibility
+  environment.systemPackages = with pkgs; [
+    vulkan-tools       # provides vulkaninfo
+    glxinfo            # from mesa-demos
+  ];
 }
