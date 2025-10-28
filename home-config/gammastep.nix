0b06b454578adcff03783eee9fd65328eb0036c4
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-28 18:18:17 +0100

    upgrade 2025-10-28 18:18

--- home-config/gammastep.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, latitude, longitude, ... }:
-{
-  services.gammastep = {
-    enable = true;
-    inherit latitude longitude;
-    temperature.day = 6000;
-    temperature.night = 3700;
-  };
-}
