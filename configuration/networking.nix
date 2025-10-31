938965a78f60e80ac42c04e96cda829c723f5b0f
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-31 22:17:46 +0100

    upgrade 2025-10-31 22:17

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -5,6 +5,15 @@
     hostName = host;
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
-    firewall.enable = true;
+    firewall {
+      enable = ture;
+      allowedTCPPorts = [];
+      allowedTCPPortRanges = [
+        { from = 5900; to = 5900; }
+      ];
+      extraCommands = ''
+        iptables -A nixos-fw -p tcp --dport 5900 -s 192.168.0.0/24 -j nixos-fw-accept
+      '';
+    };
   };
 }
