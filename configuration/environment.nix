91b5d4afccdb1b3d6ee2857f92ba2bcf41e780f2
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-10-31 18:06:14 +0100

    upgrade 2025-10-31 18:06

--- configuration/environment.nix
+++ configuration/environment.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    xfce.thunar
     (writeShellScriptBin "kbm" ''
       path="/sys/class/leds/tpacpi::kbd_backlight/brightness"
       max_path="/sys/class/leds/tpacpi::kbd_backlight/max_brightness"
--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@ let
     "firefox.nix"
     "swww.nix"
     "mango.nix"
-    "thunar.nix"
   ];
 
   modules = map (file: homeConfigurationPath + ("/" + file)) moduleFiles;
