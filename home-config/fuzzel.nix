bd148b1a1a1622b0f80d2d3d971e4dd2f46f7343
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-11-12 19:04:11 +0100

    upgrade 2025-11-12 19:04

--- home-config/fuzzel.nix
+++ home-config/fuzzel.nix
@@ -1,16 +1,32 @@
-{ config, pkgs, ... }:
-{
-  programs.fuzzel = {
-    enable = true;
+#{ config, pkgs, ... }:
+#{
+#  programs.fuzzel = {
+#    enable = true;
+#    settings = {
+#      colors = {
+#        background = "#5f5fffff";
+#	selection = "#ff5fd7ff";
+#	text = "#ffffffff";
+#	selection-text = "#ffffffff";
+#	prompt = "#ffffffff";
+#	input = "#ffffffff";
+#      };
+#    };
+#  };
+#}
+
+(wrappers.wrapperModules.fuzzel.apply
+  {
+    inherit pkgs;
     settings = {
       colors = {
         background = "#5f5fffff";
-	selection = "#ff5fd7ff";
-	text = "#ffffffff";
-	selection-text = "#ffffffff";
-	prompt = "#ffffffff";
-	input = "#ffffffff";
+        selection = "#ff5fd7ff";
+        text = "#ffffffff";
+        selection-text = "#ffffffff";
+        prompt = "#ffffffff";
+        input = "#ffffffff";
       };
     };
-  };
-}
+  }
+).wrapper
