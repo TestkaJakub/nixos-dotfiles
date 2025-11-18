8a51b2920f12f66f9f8753105cfc07c2fe472a4f
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-11-18 19:50:16 +0100

    upgrade 2025-11-18 19:50

--- wraps/fuzzel.nix
+++ wraps/fuzzel.nix
@@ -1,15 +1,21 @@
-{ wrappers, pkgs, ...}:
+{ wrappers, pkgs, theme, ...}:
 (wrappers.wrapperModules.fuzzel.apply
 {
   inherit pkgs;
   settings = {
     colors = {
-      background = "#5f5fffff";
-      selection = "#ff5fd7ff";
-      text = "#ffffffff";
-      selection-text = "#ffffffff";
-      prompt = "#ffffffff";
-      input = "#ffffffff";
+      background = theme.functions.toFuzzel theme.palette.fuzzelBackground;
+      selection = theme.functions.toFuzzel theme.palette.fuzzelSelection;
+      text = theme.functions.toFuzzel theme.palette.fuzzelText;
+      selection-text = theme.functions.toFuzzel theme.palette.fuzzelText;
+      prompt = theme.functions.toFuzzel theme.palette.fuzzelText;
+      input = theme.functions.toFuzzel theme.palette.fuzzelText;
+      #background = "#5f5fffff";
+      #selection = "#ff5fd7ff";
+      #text = "#ffffffff";
+      #selection-text = "#ffffffff";
+      #prompt = "#ffffffff";
+      #input = "#ffffffff";
     };
   };
 }
