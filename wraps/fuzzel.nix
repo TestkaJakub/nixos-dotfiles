4f7215851163ec2dd5ec9b3817a19872bc87f956
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-11-18 20:45:08 +0100

    upgrade 2025-11-18 20:45

--- wraps/fuzzel.nix
+++ wraps/fuzzel.nix
@@ -1,4 +1,7 @@
 { wrappers, pkgs, theme, ...}:
+let
+  darkenedText = theme.functions.darken theme.palette.text 0.1;
+in
 (wrappers.wrapperModules.fuzzel.apply
 {
   inherit pkgs;
@@ -7,7 +10,7 @@
       background = theme.functions.toFuzzel theme.palette.secondary;
       selection = theme.functions.toFuzzel theme.palette.primary;
       #text = theme.functions.toFuzzel (theme.functions.darken theme.palette.text 0.01);
-      text = theme.functions.toFuzzel (theme.functions.darken "#ffffff" 0.01);
+      text = theme.functions.toFuzzel darkenedText;
       selection-text = theme.functions.toFuzzel theme.palette.text;
       prompt = theme.functions.toFuzzel theme.palette.text;
       input = theme.functions.toFuzzel theme.palette.text;
