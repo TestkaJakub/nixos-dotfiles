20552cd78ba74837a201363f8126cd538ec1bf8f
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-11-18 20:25:00 +0100

    upgrade 2025-11-18 20:25

--- wraps/fuzzel.nix
+++ wraps/fuzzel.nix
@@ -7,16 +7,10 @@
       background = theme.functions.toFuzzel theme.palette.secondary;
       selection = theme.functions.toFuzzel theme.palette.primary;
       #text = theme.functions.toFuzzel (theme.functions.darken theme.palette.text 0.1);
-      text = "#ffffffff";
+      text = theme.functions.darken "#ffffffff" 0.5;
       selection-text = theme.functions.toFuzzel theme.palette.text;
       prompt = theme.functions.toFuzzel theme.palette.text;
       input = theme.functions.toFuzzel theme.palette.text;
-      #background = "#5f5fffff";
-      #selection = "#ff5fd7ff";
-      #text = "#ffffffff";
-      #selection-text = "#ffffffff";
-      #prompt = "#ffffffff";
-      #input = "#ffffffff";
     };
   };
 }
