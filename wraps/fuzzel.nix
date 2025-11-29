4ffd431d7ef864355eec2f0b4c1257c3f5f7d3ef
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-11-29 22:26:32 +0100

    upgrade 2025-11-29 22:26

--- wraps/fuzzel.nix
+++ wraps/fuzzel.nix
@@ -11,7 +11,7 @@ in
       selection = theme.functions.toFuzzel theme.palette.primary;
       #text = theme.functions.toFuzzel (theme.functions.darken theme.palette.text 0.01);
       text = theme.functions.toFuzzel darkenedText;
-      selection-text = theme.functions.toFuzzel theme.palette.text;
+      selection-text = theme.functions.toFuzzel (theme.functions.textcolor theme.palette.primary;
       prompt = theme.functions.toFuzzel theme.palette.text;
       input = theme.functions.toFuzzel theme.palette.text;
     };
