d6222053af2d1cc047148ddc9f427c956932ea07
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-12-27 16:41:11 +0100

    upgrade 2025-12-27 16:41

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -9,7 +9,7 @@
       allowedTCPPorts = [5900 8081 8082 19000 19001];
       extraCommands = ''
       	for port in 5900 8081 8082 19000 19001; do
-          iptables -A nixos-fw -p tcp -s 192.168.0.0/24 --dport $port -s 192.168.0.0/24 -j nixos-fw-accept
+          iptables -A nixos-fw -p tcp -s 192.168.0.0/24 --dport $port -j nixos-fw-accept
 	done
       '';
     };
