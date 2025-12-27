bd0034f47f36543b5ba10bb38b38d9af76fbb422
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2025-12-27 15:14:13 +0100

    upgrade 2025-12-27 15:14

--- flakes/node/flake.nix
+++ flakes/node/flake.nix
@@ -10,23 +10,31 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+
+        # wrapper that replaces the absent SDK adb
+        adbWrapper = pkgs.writeShellScriptBin "adb" ''
+          exec ${pkgs.android-tools}/bin/adb "$@"
+        '';
       in {
         devShells.default = pkgs.mkShell {
           name = "node-dev-env";
-          packages = with pkgs; [ 
-	    nodejs_22
-	    vscodium
-	    android-tools
-	  ];
+          packages = with pkgs; [
+            nodejs_22
+            vscodium
+            android-tools
+            adbWrapper      # include the wrapper in PATH
+          ];
 
           shellHook = ''
             export PS1="(node-env) $PS1"
-	    export ADB=${pkgs.android-tools}/bin/adb
-	    export ANDROID_HOME=$HOME/Android/Sdk
-	    export ANDROID_SDK_ROOT=$HOME/Android/Sdk
-	    export PATH=${pkgs.android-tools}/bin:$PATH
+            export ANDROID_HOME=$HOME/Android/Sdk
+            export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+            export PATH=${pkgs.android-tools}/bin:${pkgs.android-tools}/libexec:$PATH
 
-	    export REACT_NATIVE_ADB_PATH=${pkgs.android-tools}/bin/adb
+            # also mirror the path inside SDK for Expo hardcoding
+            mkdir -p $HOME/Android/Sdk/platform-tools
+            ln -sf ${pkgs.android-tools}/bin/adb $HOME/Android/Sdk/platform-tools/adb
+
             codium "$PWD" &
           '';
         };
