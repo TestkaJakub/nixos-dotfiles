cf90b19e3822cb1302ffbc15ef667dc3a085695a
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-01-09 16:37:12 +0100

    upgrade 2026-01-09 16:37

--- home.nix
+++ home.nix
@@ -39,7 +39,6 @@ activation.installProtonGE = lib.hm.dag.entryAfter ["writeBoundary"] ''
 '';
 
     packages = with pkgs; [
-      bambu-studio
       android-studio
       grim
       slurp
@@ -76,6 +75,30 @@ activation.installProtonGE = lib.hm.dag.entryAfter ["writeBoundary"] ''
         grim ~/Pictures/screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
         notify-send "✅ Fullscreen screenshot saved"
       '')
+    ];
+  };
+
+  bambu-studio-appimage = pkgs.appimageTools.wrapType2 rec {
+    name = "BambuStudio";
+    pname = "bambustudio";
+    version = "22.04";
+    src = pkgs.fetchurl {
+      url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}.00.70/Bambu_Studio_linux_ubuntu_${version}_PR-8834.AppImage";
+      sha256 = "sha256-qxV6pn1OVU/IGS2Lv954gm9ud1MtBr8Khdy2tJvkwj8=";
+    };
+    profile = ''
+      export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+      export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
+    '';
+    extraPkgs = pkgs: with pkgs; [
+      cacert
+      curl
+      glib
+      glib-networking
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      webkitgtk
     ];
   };
 
