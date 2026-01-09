975dd9d97dba726f3d53b380ae3dca3acef3ba10
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-01-09 14:30:55 +0100

    upgrade 2026-01-09 14:30

--- home-config/bambu-studio.nix
+++ home-config/bambu-studio.nix
@@ -2,7 +2,8 @@
 
 let
   bambuStudio = pkgs.appimageTools.wrapType2 {
-    name = "bambu-studio";
+    pname = "bambu-studio";
+    version = "2.04.00.70";
     src = pkgs.fetchurl {
       url = "https://github.com/bambulab/BambuStudio/releases/download/v02.04.00.70/Bambu_Studio_ubuntu-22.04_PR-8834.AppImage";
       sha256 = "ff17150f760fb80afc98d9841b134c0bada4897d6aaf36808b517a4bed2c11b0";
