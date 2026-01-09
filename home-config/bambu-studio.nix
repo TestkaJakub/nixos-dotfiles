1091fa2482c767d422ee08257cd58550fe60fbce
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-01-09 15:33:35 +0100

    upgrade 2026-01-09 15:33

--- home-config/bambu-studio.nix
+++ home-config/bambu-studio.nix
@@ -1,67 +1,40 @@
 { pkgs, ... }:
 
 let
-  bambuStudio = pkgs.appimageTools.wrapType2 {
-    pname = "bambu-studio";
-    version = "2.04.00.70";
+  appImagePath = pkgs.fetchurl {
+    url = "https://github.com/bambulab/BambuStudio/releases/download/v02.04.00.70/Bambu_Studio_ubuntu-22.04_PR-8834.AppImage";
+    sha256 = "ff17150f760fb80afc98d9841b134c0bada4897d6aaf36808b517a4bed2c11b0";
+  };
 
-    src = pkgs.fetchurl {
-      url = "https://github.com/bambulab/BambuStudio/releases/download/v02.04.00.70/Bambu_Studio_ubuntu-22.04_PR-8834.AppImage";
-      sha256 = "ff17150f760fb80afc98d9841b134c0bada4897d6aaf36808b517a4bed2c11b0";
-    };
-
-    extraPkgs = pkgs: with pkgs; [
-          # Base system libs
+  bambuStudioFhs = pkgs.buildFHSUserEnv {
+    name = "bambu-studio-fhs";
+    targetPkgs = pkgs: with pkgs; [
+      # Core system and GUI runtime
       stdenv.cc.cc.lib
-      zlib
-      fontconfig
-      freetype
-
-      # Core graphics
-      mesa
-      libGL
-      cairo
-      pango
-      atk
-      gdk-pixbuf
-      gtk3
-
-      # WebKit + runtime
-      webkitgtk_4_1
-      libsoup_3
-      enchant2
-      harfbuzz
-      icu
-      gst_all_1.gstreamer
-      gst_all_1.gst-libav
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad
-      ffmpeg
-
-      # X11 / Wayland / IPC
-      xorg.libX11
-      xorg.libXext
-      xorg.libxcb
-      libxkbcommon
-      dbus
-      at-spi2-core
-      wayland
-
-      # Misc runtime utilities
-      glib
-      gsettings-desktop-schemas
+      zlib fontconfig freetype mesa libGL glib gtk3 gdk-pixbuf pango cairo atk
+      webkitgtk_4_1 libsoup_3 enchant2 harfbuzz icu
+      gst_all_1.gstreamer gst_all_1.gst-libav gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good gst_all_1.gst-plugins-bad ffmpeg
+      xorg.libX11 xorg.libXext xorg.libxcb wayland libxkbcommon
+      at-spi2-core dbus gsettings-desktop-schemas
     ];
+    runScript = pkgs.writeScript "bambu-studio-run" ''
+      #!/usr/bin/env bash
+      tmpApp="/tmp/bambu-studio.AppImage"
+      cp ${appImagePath} "$tmpApp"
+      chmod +x "$tmpApp"
+      exec "$tmpApp" "$@"
+    '';
   };
 in {
-  home.packages = [ bambuStudio ];
+  home.packages = [ bambuStudioFhs ];
 
   xdg.desktopEntries.bambu-studio = {
-    name = "Bambu Studio";
+    name = "Bambu Studio (FHS)";
     comment = "3D printing software for Bambu Lab printers";
-    exec = "env GDK_BACKEND=x11 QT_QPA_PLATFORM=xcb ${bambuStudio}";
-    icon = "${bambuStudio}/share/icons/hicolor/256x256/apps/bambu-studio.png";
-    categories = [ "Graphics" "Engineering" "X-3DPrinting" ];
+    exec = "env QT_QPA_PLATFORM=xcb ${bambuStudioFhs}/bin/bambu-studio-fhs";
+    icon = "application-x-executable";
+    categories = [ "Graphics" "Engineering" ];
     type = "Application";
     terminal = false;
   };
