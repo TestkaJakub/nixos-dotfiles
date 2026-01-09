2361b4f61530f51edf5f323abffa2e2242a93eb8
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-01-09 18:55:02 +0100

    upgrade 2026-01-09 18:55

--- configuration/system.nix
+++ configuration/system.nix
@@ -3,7 +3,7 @@
 {
   i18n = {
     defaultLocale = "en_US.UTF-8";
-    extraLocales = [ "pl_PL.UTF-8/UTF-8", "en_GB.UTF-8/UTF-8" ];
+    extraLocales = [ "pl_PL.UTF-8/UTF-8" "en_GB.UTF-8/UTF-8" ];
   };
 
   console.keyMap = "pl2";
