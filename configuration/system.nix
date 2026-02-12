032977ba60be4870068cd4651db427c431eb279c
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-02-12 17:26:10 +0100

    upgrade 2026-02-12 17:26

--- configuration/system.nix
+++ configuration/system.nix
@@ -41,15 +41,7 @@
     ];
   };
 
-     services.avahi = {
-      enable = true;
-      nssmdns = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        userServices = true;
-      };
-    };
+
 
   nixpkgs.config.allowUnfree = true;
 
