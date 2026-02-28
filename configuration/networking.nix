f0ac967c3203a4fa414d555a105b01e8165d5163
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-02-28 19:05:40 +0100

    upgrade 2026-02-28 19:05

--- configuration/networking.nix
+++ configuration/networking.nix
@@ -6,7 +6,7 @@
     useDHCP = lib.mkDefault true;
     firewall = {
       enable = true;
-      allowedTCPPorts = [5900 8081 8082 19000 19001];
+      allowedTCPPorts = [ 3456 5900 8081 8082 19000 19001 ];
       extraCommands = ''
       	for port in 5900 8081 8082 19000 19001; do
           iptables -A nixos-fw -p tcp -s 192.168.0.0/24 --dport $port -j nixos-fw-accept
