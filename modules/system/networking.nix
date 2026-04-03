1594d6cc8bf2c94599411daab797468f85188bf1
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-04-03 17:32:14 +0200

    upgrade 2026-04-03 17:32

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -5,7 +5,7 @@
   networking = {
     networkmanager = { 
     	enable = true;
-    	insertNameservers = [ "192.168.0.252" ];
+    	dns = "none";
    	};
     useDHCP               = lib.mkDefault true;
     nameservers = [ "192.168.0.252" "1.1.1.1" ];
