29dfb58a4a4faf34ee6985e4c521df04e8a6f858
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-04-14 21:46:54 +0200

    upgrade 2026-04-14 21:46

--- modules/media/jellyfin.nix
+++ modules/media/jellyfin.nix
@@ -1,19 +1,7 @@
 { pkgs, config, ... }:
 let
   user = config.profile.username;
-  bundle = pkgs.runCommand "combined-ca-bundle-v3" {} ''
-    cat ${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt \
-        ${../meta/homelab-root.crt} \
-        ${../meta/homelab-intermediate.crt} > $out
-  '';
-  shim = pkgs.jellyfin-mpv-shim.overrideAttrs (old: {
-    makeWrapperArgs = (old.makeWrapperArgs or []) ++ [
-      "--set" "WEBSOCKET_CLIENT_CA_BUNDLE" "${bundle}"
-      "--set" "REQUESTS_CA_BUNDLE" "${bundle}"
-      "--set" "SSL_CERT_FILE" "${bundle}"
-    ];
-  });
 in
 {
-  home-manager.users.${user}.home.packages = [ shim ];
+  home-manager.users.${user}.home.packages = [ pkgs.jellyfin-mpv-shim ];
 }
