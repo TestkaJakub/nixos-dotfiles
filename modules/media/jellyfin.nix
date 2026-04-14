a437d70680cbda7e9098dafdff5f2b3698cbfa3a
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-04-14 19:52:57 +0200

    upgrade 2026-04-14 19:52

--- modules/media/jellyfin.nix
+++ modules/media/jellyfin.nix
@@ -3,7 +3,7 @@ let
   user = config.profile.username;
   shim = pkgs.jellyfin-mpv-shim;
   wrapper = pkgs.writeShellScriptBin "jellyfin-mpv-shim" ''
-    export SSL_CERT_FILE="/home/${config.profile.username}/.step/certs/root_ca.crt"
+    export SSL_CERT_FILE="${config.security.pki.caBundle}"
     exec ${shim}/bin/jellyfin-mpv-shim "$@"
   '';
 in
