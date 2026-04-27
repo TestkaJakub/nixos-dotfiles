ad8cbd4fb77f3c146f04da6b8cec970e68dab55b
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-04-27 21:11:30 +0200

    upgrade 2026-04-27 21:11

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    mangowc.url      = "github:DreamMaoMao/mangowc";
+    mangowc.url = "github:DreamMaoMao/mangowc/3fa306fc191fd70ea2cdb8f2328685af7d44e419";
     wrappers.url     = "github:lassulus/wrappers";
   };
 
