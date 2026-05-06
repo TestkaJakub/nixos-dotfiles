35f7edeedb6eb7c00c69c8e6b97b0c0d84b3bee3
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-05-06 17:18:47 +0200

    upgrade 2026-05-06 17:18

--- flake.nix
+++ flake.nix
@@ -9,8 +9,8 @@
       url            = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    mangowc.url = "github:DreamMaoMao/mangowc/3fa306fc191fd70ea2cdb8f2328685af7d44e419";
+	mangowc.url = "github:DreamMaoMao/mangowc/42c02e3dc20eb09c0191b027e387c0268f8e0fb5";
+    #mangowc.url = "github:DreamMaoMao/mangowc/3fa306fc191fd70ea2cdb8f2328685af7d44e419";
     wrappers.url     = "github:lassulus/wrappers";
   };
 
