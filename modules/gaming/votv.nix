7177196012a329b6654ce979935a39f1d8bdcc05
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-05-06 20:11:18 +0200

    upgrade 2026-05-06 20:11

--- modules/gaming/votv.nix
+++ modules/gaming/votv.nix
@@ -1,10 +1,9 @@
 { pkgs, ... }:
-
 {
   environment.systemPackages = [
     (pkgs.stdenv.mkDerivation {
       pname   = "yeetpatch";
-      version = "latest3";
+      version = "latest4";
 
       src = pkgs.fetchurl {
         url    = "https://votv.dev/patcher_assets/download/YeetPatch-latest-linux.tar";
@@ -12,21 +11,22 @@
       };
 
       sourceRoot = ".";
-      
-	  nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
 
-		installPhase = ''
-		  set -x
-		  mkdir -p $out/bin
-		  cp YeetPatch.sh $out/bin/yeetpatch
-		  grep "DESYNC_BIN" $out/bin/yeetpatch
-		  substitute $out/bin/yeetpatch $out/bin/yeetpatch \
-		    --replace-fail 'DESYNC_BIN="$SCRIPT_DIR/desync"' 'DESYNC_BIN="desync"'
-		  grep "DESYNC_BIN" $out/bin/yeetpatch
-		  chmod +x $out/bin/yeetpatch
-		  wrapProgram $out/bin/yeetpatch \
-		    --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jq pkgs.curl pkgs.desync ]}
-		'';
+      nativeBuildInputs = [ pkgs.makeBinaryWrapper ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        substitute YeetPatch.sh $out/bin/yeetpatch \
+          --replace-fail \
+            'DESYNC_BIN="$SCRIPT_DIR/desync"' \
+            'DESYNC_BIN="$(command -v desync)"' \
+          --replace-fail \
+            'if [[ -x "$DESYNC_BIN" ]]; then' \
+            'if [[ -n "$DESYNC_BIN" ]]; then'
+        chmod +x $out/bin/yeetpatch
+        wrapProgram $out/bin/yeetpatch \
+          --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jq pkgs.curl pkgs.desync ]}
+      '';
     })
   ];
 }
