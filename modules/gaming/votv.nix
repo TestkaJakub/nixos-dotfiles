c32e69e399138dcda844973753d9c85c571c00d7
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-05-06 20:16:36 +0200

    upgrade 2026-05-06 20:16

--- modules/gaming/votv.nix
+++ modules/gaming/votv.nix
@@ -16,16 +16,10 @@
 
       installPhase = ''
         mkdir -p $out/bin
-        substitute YeetPatch.sh $out/bin/yeetpatch \
-          --replace-fail \
-            'DESYNC_BIN="$SCRIPT_DIR/desync"' \
-            'DESYNC_BIN="$(command -v desync)"' \
-          --replace-fail \
-            'if [[ -x "$DESYNC_BIN" ]]; then' \
-            'if [[ -n "$DESYNC_BIN" ]]; then'
-          --replace-fail \
-            'INSTALL_DIR=""' \
-            'INSTALL_DIR="''${INSTALL_DIR:-}"'
+substitute YeetPatch.sh $out/bin/yeetpatch \
+  --replace 'DESYNC_BIN="$SCRIPT_DIR/desync"' 'DESYNC_BIN="$(command -v desync)"' \
+  --replace 'if [[ -x "$DESYNC_BIN" ]]; then' 'if [[ -n "$DESYNC_BIN" ]]; then' \
+  --replace 'INSTALL_DIR=""' 'INSTALL_DIR="''${INSTALL_DIR:-}"'
         chmod +x $out/bin/yeetpatch
         wrapProgram $out/bin/yeetpatch \
           --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.jq pkgs.curl pkgs.desync ]}
