d79baf0edd0aaa5aa34f7d828e06b7eda2748e16
Jakub Testka <84832891+TestkaJakub@users.noreply.github.com>
2026-05-06 16:52:09 +0200

    upgrade 2026-05-06 16:52

--- flake.nix
+++ flake.nix
@@ -45,7 +45,6 @@
 
       # ── Patched pkgs ────────────────────────────────────────────────────────
       pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
         overlays = [];
         config.allowUnfree = true;
       };
