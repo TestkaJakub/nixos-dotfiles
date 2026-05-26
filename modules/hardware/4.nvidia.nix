b981c23506643cda56dbd5282dd07502b5055ff4
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-05-26 22:24:43 +0200

    upgrade 2026-05-26 22:24

---
+++ modules/hardware/4.nvidia.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, config, ... }:
+
+{
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable  = true;
+    powerManagement.enable = false;
+    open   = false;          # GTX 660 is Kepler — must use closed source
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    # GTX 660 (GK106) needs the 470.xx legacy driver
+  };
+
+  environment.systemPackages = [ pkgs.nvtopPackages.nvidia ];
+}
