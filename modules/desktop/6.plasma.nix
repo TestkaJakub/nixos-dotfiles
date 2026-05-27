ae009331b46c65b4d256adbc8b5b5e400135b11a
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-05-27 23:13:06 +0200

    upgrade 2026-05-27 23:13

--- modules/desktop/6.plasma.nix
+++ modules/desktop/6.plasma.nix
@@ -7,24 +7,29 @@
     displayManager.sddm.wayland.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    # KDE Utilities
-    kdePackages.discover # Optional: Software center for Flatpaks/firmware updates
-    kdePackages.kcalc # Calculator
-    kdePackages.kcharselect # Character map
-    kdePackages.kclock # Clock app
-    kdePackages.kcolorchooser # Color picker
-    kdePackages.kolourpaint # Simple paint program
-    kdePackages.ksystemlog # System log viewer
-    kdePackages.sddm-kcm # SDDM configuration module
-    kdiff3 # File/directory comparison tool
+  environment = { 
+  	systemPackages = with pkgs; [
+    	# KDE Utilities
+	    kdePackages.discover # Optional: Software center for Flatpaks/firmware updates
+   		kdePackages.kcalc # Calculator
+   		kdePackages.kcharselect # Character map
+    	kdePackages.kclock # Clock app
+    	kdePackages.kcolorchooser # Color picker
+    	kdePackages.kolourpaint # Simple paint program
+    	kdePackages.ksystemlog # System log viewer
+    	kdePackages.sddm-kcm # SDDM configuration module
+    	kdiff3 # File/directory comparison tool
     
-    # Hardware/System Utilities (Optional)
-    kdePackages.isoimagewriter # Write hybrid ISOs to USB
-    kdePackages.partitionmanager # Disk and partition management
-    hardinfo2 # System benchmarks and hardware info
-    wayland-utils # Wayland diagnostic tools
-    wl-clipboard # Wayland copy/paste support
-    vlc # Media player
-  ];
+    	# Hardware/System Utilities (Optional)
+    	kdePackages.isoimagewriter # Write hybrid ISOs to USB
+    	kdePackages.partitionmanager # Disk and partition management
+    	hardinfo2 # System benchmarks and hardware info
+    	wayland-utils # Wayland diagnostic tools
+    	wl-clipboard # Wayland copy/paste support
+    	vlc # Media player
+ 	 ];
+ 	 variables = {
+ 	 	KWIN_DRM_DEVICES = "dev/dri/card1";
+ 	 };
+  };
 }
