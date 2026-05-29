29fd226aa80a54a7cd5ab0fbe1571ed664121edd
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-05-29 17:48:11 +0200

    upgrade 2026-05-29 17:48

--- modules/webbrowsers/6.vivaldi.nix
+++ modules/webbrowsers/6.vivaldi.nix
@@ -2,13 +2,20 @@
 {
   environment.systemPackages = [
     (pkgs.vivaldi.override {
+      proprietaryCodecs = true;
+      enableWidevine     = true;
       commandLineArgs = [
         "--ozone-platform=x11"
         "--gtk-version=4"
       ];
+      # Force Vivaldi to use system GTK instead of its bundled one
+      vivaldi = pkgs.vivaldi.overrideAttrs (old: {
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.makeWrapper ];
+        postFixup = (old.postFixup or "") + ''
+          wrapProgram $out/bin/vivaldi \
+            --unset LD_LIBRARY_PATH
+        '';
+      });
     })
   ];
-  environment.sessionVariables = {
-    GTK_USE_PORTAL = "0";
-  };
 }
