255dbc03c6c1b8a88ccb5642df36afd3a1bb87f3
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-05-29 09:24:43 +0200

    upgrade 2026-05-29 09:24

--- modules/desktop/6.lxqt.nix
+++ modules/desktop/6.lxqt.nix
@@ -1,17 +1,12 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   services.xserver.enable = true;
 
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.lxqt.enable    = true;
 
-  # Ensure Plasma/KDE is fully absent
-  services.xserver.desktopManager.plasma6.enable = lib.mkForce false;
-  services.displayManager.sddm.enable            = lib.mkForce false;
-
   environment.systemPackages = with pkgs; [
     lxqt.lxqt-archiver
-    lxqt.lxqt-calendar
     lxqt.lximage-qt
     lxqt.qps
     lxqt.screengrab
