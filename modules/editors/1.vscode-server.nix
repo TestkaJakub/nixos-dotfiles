4806dde60783f10e53fedeee65f46e0bcf873332
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-05-30 13:32:28 +0200

    upgrade 2026-05-30 13:32

--- modules/editors/1.vscode-server.nix
+++ modules/editors/1.vscode-server.nix
@@ -3,5 +3,8 @@
 # ── VSCode/VSCodium remote server ──────────────────────────────────────────────
 # Enables Open Remote SSH support from VSCodium on the workstation.
 {
-  services.vscode-server.enable = true;
+  services.vscode-server = {
+    enable = true;
+    installPath = "$HOME/.vscodium-server";
+  };
 }
