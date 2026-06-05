49fe051911f92e8b78c5810bef9cc5ed0f589e9f
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-06-05 09:15:48 +0200

    upgrade 2026-06-05 09:15

--- modules/meta/6.scripts.nix
+++ modules/meta/6.scripts.nix
@@ -58,6 +58,7 @@ in
           SOURCE=$(${pkgs.zenity}/bin/zenity \
             --file-selection \
             --title="Select wallpaper" \
+            --filename="$HOME/.config/wallpaper/collection/" \
             --file-filter="Images|*.jpg *.jpeg *.png *.bmp *.gif *.tiff *.webp" \
             2>/dev/null) || { echo "Cancelled."; exit 0; }
 
