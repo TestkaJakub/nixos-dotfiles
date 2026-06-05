c08ded48d63648b83b6b03017b6f87d4cc8d846d
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-06-05 16:51:03 +0200

    upgrade 2026-06-05 16:51

--- modules/webbrowsers/6.brave.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-
-# ── Brave ──────────────────────────────────────────────────────────────────────
-# System package — available to all users and on the login screen.
-{
-  environment.systemPackages = [ pkgs.brave ];
-}
