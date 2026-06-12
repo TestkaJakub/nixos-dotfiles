2e5c7f34ff39908a114e7ee0f73db4a095d89c70
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-06-12 18:19:06 +0200

    upgrade 2026-06-12 18:19

--- modules/desktop/6.i3.nix
+++ modules/desktop/6.i3.nix
@@ -60,7 +60,6 @@ in
     vlc
     hardinfo2
     imv
-    nm-applet
   ];
 
   home-manager.users.${user} = { lib, ... }: {
@@ -106,7 +105,7 @@ in
       exec_always --no-startup-id ${config.scripts.wallpaperInit}/bin/wallpaper-init
       exec_always --no-startup-id ${pkgs.picom}/bin/picom --daemon
       exec_always --no-startup-id bash -c 'pkill polybar; until [ -S /run/user/$(id -u)/i3/ipc-socket.* ] 2>/dev/null; do sleep 0.1; done; ${polybar}/bin/polybar main'
-	  exec_always --no-startup-id ${pkgs.nm-applet}/bin/nm-applet
+	  exec_always --no-startup-id ${pkgs.networkmanagerapplet}/bin/nm-applet
       exec        --no-startup-id ${pkgs.udiskie}/bin/udiskie --tray &
       exec        --no-startup-id ${pkgs.dunst}/bin/dunst &
 
