cd0ee6e90970a465cd1e747ac334fdb8a034a7f2
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-08-22 01:01:09 +0200

    upgrade 2026-08-22 01:01

--- modules/virtualisation/7.docker.nix
+++ modules/virtualisation/7.docker.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  isDesktop = config.profile.isRole [ "desktop" ];
+in
 {
   virtualisation.docker = {
     enable = true;
     daemon.settings = {
       userland-proxy = true;
       experimental   = true;
+      dns = lib.mkIf isDesktop [ "1.1.1.1" "8.8.8.8" ];
     };
   };
 
