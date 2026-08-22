271d91b09fe56f099474e415b0c8d6c7d798ba2d
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-08-22 10:28:35 +0200

    upgrade 2026-08-22 10:28

--- modules/containers/7.ollama.nix
+++ modules/containers/7.ollama.nix
@@ -13,8 +13,9 @@ in
     "d /home/jakub/data/ollama-store 0755 root root - -"
   ];
 
-  fileSystems."/var/lib/ollama" = {
+  fileSystems."/var/lib/private/ollama" = {
     device  = "/home/jakub/data/ollama-store";
-    options = [ "bind" "x-systemd.requires=home-jakub-data.mount" ];
+    options = [ "bind" "nofail" ];
+    depends = [ "/home/jakub/data" ];
   };
 }
