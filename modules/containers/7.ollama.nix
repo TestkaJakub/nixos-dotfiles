f84112c8177e537fa3cd981a9dbb9cb55acd3c0a
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-08-22 10:19:09 +0200

    upgrade 2026-08-22 10:19

--- modules/containers/7.ollama.nix
+++ modules/containers/7.ollama.nix
@@ -7,16 +7,14 @@ in
     enable = true;
     host   = "0.0.0.0";
     acceleration = lib.mkIf isDesktop "rocm";
-    models = "/home/jakub/data/ollama-models";
-  };
-  
-  fileSystems."/var/lib/ollama-models" = {
-    device  = "/home/jakub/data/ollama-models";
-    options = [ "bind" ];
   };
 
   systemd.tmpfiles.rules = [
-    "d /home/jakub/data/ollama-models 0755 root root - -"
-    "d /var/lib/ollama-models         0755 root root - -"
+    "d /home/jakub/data/ollama-store 0755 root root - -"
   ];
+
+  fileSystems."/var/lib/ollama" = {
+    device  = "/home/jakub/data/ollama-store";
+    options = [ "bind" "x-systemd.requires=home-jakub-data.mount" ];
+  };
 }
